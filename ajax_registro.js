d8a42f17fa618f0244779380ba272839e6da3982
Christhian Aracena <christhian.aracena@correoaiep.cl>
2024-03-11 01:52:39 -0300

    segundo avance backend

--- ajax_registro.js
+++ ajax_registro.js
@@ -16,6 +16,8 @@ function submitForm() {
                 success: function(response) {
                     if (response === 'success') {
                         openSuccessModal();
+                    } else if (response === 'duplicate_email') {
+                        showAlert('El correo electrónico ya existe. Por favor, elija otro.');
                     } else {
                         alert('Error al registrar la cuenta. Inténtelo de nuevo.');
                     }
@@ -26,6 +28,29 @@ function submitForm() {
         form.reportValidity();
     }
 }
+
+function showAlert(message) {
+    // Elimina las alertas antiguas antes de mostrar una nueva
+    $('.alert').remove();
+
+    var alertBox = $('<div class="alert closeable">').text(message);
+    var closeButton = $('<span class="close">&times;</span>');
+    alertBox.append(closeButton);
+    $('body').append(alertBox);
+
+    alertBox.css('display', 'block'); // Mostramos la alerta
+    alertBox.css('animation', 'slideIn 0.5s forwards'); // Aplicamos la animación de desplazamiento hacia abajo
+
+    closeButton.on('click', function() {
+        alertBox.fadeOut();
+    });
+
+    // Desaparecer la alerta después de 3 segundos
+    setTimeout(function() {
+        alertBox.fadeOut();
+    }, 3000);
+}
+
 
 function openSuccessModal() {
     $('#successModal').show();
--- procesar_registro.php
+++ procesar_registro.php
@@ -7,12 +7,23 @@ $correo = $_POST['correo_registro'];
 $ocupacion = $_POST['ocupacion'];
 $password = $_POST['password'];
 
+// Consulta para verificar si el correo ya existe
+$check_query = "SELECT COUNT(*) AS count FROM usuario_login WHERE correo = '$correo'";
+$result = mysqli_query($conexion, $check_query);
+$row = mysqli_fetch_assoc($result);
+$count = $row['count'];
 
-$query = "INSERT INTO usuario_login (nombre_login, apellido_login, correo, contrasena, ocupacion_id, rol_id) VALUES ('$nombre', '$apellido', '$correo', '$password', $ocupacion, 1)";
-
-if(mysqli_query($conexion, $query)) {
-    echo 'success';
+if ($count > 0) {
+    // El correo ya existe, mostrar alerta
+    echo 'duplicate_email';
 } else {
-    echo 'error';
+    // El correo no existe, realizar la inserción
+    $insert_query = "INSERT INTO usuario_login (nombre_login, apellido_login, correo, contrasena, ocupacion_id, rol_id) VALUES ('$nombre', '$apellido', '$correo', '$password', $ocupacion, 1)";
+
+    if(mysqli_query($conexion, $insert_query)) {
+        echo 'success';
+    } else {
+        echo 'error';
+    }
 }
 ?>
